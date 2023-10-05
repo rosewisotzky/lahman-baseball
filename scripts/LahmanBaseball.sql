06d10e9923586d2eaed5db19213dab0efaa101dc
rosewisotzky <rose@rosewisotzky.com>
2023-10-05 14:36:27 -0400

    post lunch push

--- scripts/LahmanBaseball.sql
+++ scripts/LahmanBaseball.sql
@@ -10,7 +10,12 @@ SELECT
 	MAX(yearID)
 FROM batting;
 
--- 2.Find the name and height of the shortest player in the database. How many games did he play in? What is the name of the team for which he played?
+SELECT 
+	MIN(yearID),
+	MAX(yearID)
+FROM appearances;
+
+-- 2.Find the name and height of the shortest player in the database. How many games did he play in? What is the name of the team for which he played? --NOTE TO SELF FINISH THIS!!!!!--
 
 SELECT namefirst,
 	   namelast,
@@ -22,6 +27,14 @@ FROM people
 WHERE height = (SELECT MIN(height)
 				 FROM people)
 GROUP BY namefirst, namelast, teams.name;
+
+SELECT * 
+FROM appearances
+WHERE playerID = 'gaedeed01';
+
+SELECT playerID
+FROM people
+WHERE namelast = 'Gaedel';
 
 -- 3. Find all players in the database who played at Vanderbilt University. Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
 
@@ -48,15 +61,126 @@ FROM fielding
 WHERE yearID = '2016'
 GROUP BY player_position;
 
--- 5. Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?
+-- 5. Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends? ROEY NOTE: use the teams table!
+
+SELECT 
+	CASE WHEN yearID::text LIKE '192%' THEN '1920s'
+		 WHEN yearID::text LIKE '193%' THEN '1930s'
+		 WHEN yearID::text LIKE '194%' THEN '1940s'
+		 WHEN yearID::text LIKE '195%' THEN '1950s'
+		 WHEN yearID::text LIKE '196%' THEN '1960s'
+		 WHEN yearID::text LIKE '197%' THEN '1970s'
+		 WHEN yearID::text LIKE '198%' THEN '1980s'
+		 WHEN yearID::text LIKE '199%' THEN '1990s'
+		 WHEN yearID::text LIKE '200%' THEN '2000s'
+		 WHEN yearID::text LIKE '201%' THEN '2010s'
+		 WHEN yearID::text LIKE '202%' THEN '2020s'
+		 ELSE 'no data'
+		 END AS decade,
+	ROUND(SUM((SO + SOA))/SUM(G), 2) AS avg_strikeout_per_game
+-- 	SUM(G) AS total_games
+FROM teams
+GROUP BY decade
+ORDER BY decade;
 
 
---  6. Find the player who had the most success stealing bases in 2016, where success is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted at least 20 stolen bases.
 
--- 7. From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? What is the smallest number of wins for a team that did win the world series? Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
+--  6. Find the player who had the most success stealing bases in 2016, where success is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted at least 20 stolen bases. **ROEY NOTE** Looking at batting post table? jk, batting table!
 
--- 8. Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
+SELECT playerID, 
+	   SUM(SB) AS stolen_bases,
+	   SUM(CS) AS caught_stealing,
+	   CASE WHEN (SUM(SB::decimal) > 0.00 AND SUM(CS::decimal)> 0.00) THEN ROUND(SUM(SB::decimal)/(SUM(CS::decimal) + SUM(SB::decimal)), 2) * 100 END AS percent_stolen
+FROM batting
+WHERE yearID = 2016
+GROUP BY playerID
+HAVING (SUM(SB) + SUM(CS) >= 20)
+ORDER BY percent_stolen DESC NULLS LAST;
 
+
+-- 7. From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? 
+
+SELECT
+	DISTINCT yearID,
+	MAX(W) OVER(PARTITION BY yearID)AS max_wins
+FROM teams
+WHERE yearID BETWEEN 1970 AND 2016
+	AND WSWin = 'N'
+ORDER BY max_wins DESC
+LIMIT 1;
+
+-- What is the smallest number of wins for a team that did win the world series? Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. (rose note: this was the 1981 players strike most likely) 
+
+
+SELECT
+	DISTINCT yearID,
+	MIN(W) OVER(PARTITION BY yearID)AS wins
+FROM teams
+WHERE yearID BETWEEN 1970 AND 2016
+	AND WSWin = 'Y'
+ORDER BY wins
+LIMIT 1;	
+		
+-- Then redo your query, excluding the problem year. 		
+		
+SELECT
+	DISTINCT yearID,
+	MIN(W) OVER(PARTITION BY yearID)AS wins
+FROM teams
+WHERE yearID BETWEEN 1970 AND 2016
+	AND WSWin = 'Y'
+	AND yearID <> 1981
+ORDER BY wins
+LIMIT 1; 			
+		
+
+--How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
+--what I want to find is the following: for each year between 1970 and 2016, see if the team with the most wins won the world series.
+
+WITH wins_by_series AS(SELECT
+							DISTINCT yearID,
+							CASE WHEN wswin = 'Y' THEN MAX(W)END AS max_wins_series,
+							CASE WHEN wswin <> 'Y' THEN MAX(W) END AS max_wins_no_series
+						FROM teams
+						WHERE yearID BETWEEN 1970 AND 2016
+						GROUP BY yearID, wswin)
+SELECT 
+	COUNT(max_wins_series) AS max_wins_series_count,
+	COUNT(max_wins_no_series) AS max_wins_no_series_count
+FROM wins_by_series
+WHERE max_wins_series IS NOT NULL;
+
+-- 8. Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. 
+
+SELECT 
+	parks.park_name,
+	teams.name,
+	SUM(homegames.attendance)/SUM(homegames.games) AS avg_attendance
+FROM homegames
+	INNER JOIN parks USING(park)
+	INNER JOIN teams ON teams.teamid = homegames.team
+WHERE year = 2016
+GROUP BY parks.park_name, teams.name
+HAVING SUM(games) >= 10
+ORDER BY avg_attendance DESC
+LIMIT 5;
+
+--Repeat for the lowest 5 average attendance.
+
+SELECT 
+	parks.park_name,
+	teams.name,
+	SUM(homegames.attendance)/SUM(homegames.games) AS avg_attendance
+FROM homegames
+	INNER JOIN parks USING(park)
+	INNER JOIN teams ON teams.teamid = homegames.team
+WHERE year = 2016
+GROUP BY parks.park_name, teams.name
+HAVING SUM(games) >= 10
+ORDER BY avg_attendance DESC
+LIMIT 5;
 -- 9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won the award.
+
+
 
 -- 10. Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016.
